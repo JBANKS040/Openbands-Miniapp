9a8552a791af5acdee6b358683ec9a1b5868b929
masaun <masaun@protonmail.com>
2025-09-05 12:07:51 +0900

    [FE - Wallet]: Comment out the connectToEvmWalletUsingBaseAccountSDK() in the connect-to-evm-wallet-using-base-account-sdk.ts

--- miniapp-base/src/lib/blockchains/evm/connect-wallets/connect-to-evm-wallet-using-base-account-sdk.ts
+++ miniapp-base/src/lib/blockchains/evm/connect-wallets/connect-to-evm-wallet-using-base-account-sdk.ts
@@ -5,25 +5,24 @@ import { createBaseAccountSDK, base } from '@base-org/account';
  * @notice - This would be worked with a <Wallet /> component and Wallet Modal powered by BASE's OnChainKit.
  * @dev - ref). https://docs.base.org/onchainkit/wallet/wallet
  */
-export async function connectToEvmWalletUsingBaseAccountSDK(): Promise<{ provider: any }> {
-  const sdk = createBaseAccountSDK({
-    appName: 'My App Name',
-    appLogoUrl: 'https://example.com/logo.png',
-    appChainIds: [base.constants.CHAIN_IDS.base],
-  });
+// export async function connectToEvmWalletUsingBaseAccountSDK(): Promise<{ provider: any, config: any }> {
+//   const sdk = createBaseAccountSDK({
+//     appName: 'My App Name',
+//     appLogoUrl: 'https://example.com/logo.png',
+//     appChainIds: [base.constants.CHAIN_IDS.base],
+//   });
 
-  // @dev - Get a provider from the Base Account SDK instance, which is avaiable for Wagmi
-  const provider = sdk.getProvider();
+//   // @dev - Get a provider from the Base Account SDK instance, which is avaiable for Wagmi
+//   const provider = sdk.getProvider();
 
-  // @dev - Wagmi configuration to use the provider from Base Account SDK  
-  // @dev - [TODO]: The following "config" should be finalized.
-  // const config = createConfig({
-  //   chains: [base],
-  //   transports: {
-  //     [base.id]: custom(provider),
-  //   },
-  // });
+//   // @dev - Wagmi configuration to use the provider from Base Account SDK  
+//   // @dev - [TODO]: The following "config" should be finalized.
+//   const config = createConfig({
+//     chains: [base],
+//     transports: {
+//       [base.id]: custom(provider),
+//     },
+//   });
 
-  return { provider };
-  //return { provider, config };
-}
+//   return { provider, config };
+// }
