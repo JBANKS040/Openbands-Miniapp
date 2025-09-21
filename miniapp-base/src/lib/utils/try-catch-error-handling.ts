467468c64bc58323bcb0c608580ac10f62cef3f3
masaun <masaun@protonmail.com>
2025-09-21 09:32:05 +0900

    [FE - Notification]: Add the 'export' keyword to the extractErrorMessageInString()

--- miniapp-base/src/lib/utils/try-catch-error-handling.ts
+++ miniapp-base/src/lib/utils/try-catch-error-handling.ts
@@ -2,7 +2,7 @@
  * @notice - Extracts the error message in a string format from an unknown error object.
  * @dev - This function is used in a try-catch block.
  */
-function extractErrorMessageInString(error: unknown): error is { message: string } {
+export function extractErrorMessageInString(error: unknown): error is { message: string } {
   return (
     typeof error === "object" &&
     error !== null &&
